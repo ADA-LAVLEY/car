d1efedfb0c387528f2c365440130b16be1bee70f
ADA-LAVLEY <107742280+ADA-LAVLEY@users.noreply.github.com>
2023-09-23 14:46:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,420 @@
+// console.log(12);
+// console.log('hello');
+
+// let son = 10
+// son = 11
+
+// var son1 = 20
+// son1 = 12
+
+// // const son2 = 30
+// // son2 = 33
+
+// console.log(son);
+
+// var
+// const
+
+
+// data types
+
+// let str = "Salom"  //String
+// let num = 123  // number
+// let bool = true  // false  // Boolin
+// let und = undefined // undefined
+// let nul = null  // null
+
+
+// console.log(typeof bool);
+
+// let s = 10
+// let c = "10"
+
+// console.log(s,c);
+
+// let a = 150
+// let b = 100
+
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a ** b);
+// console.log(a % b);
+
+// console.log(a === b);
+
+// console.log(a >= b);
+
+// ! - emas -- not
+// || ---- yoki --- or
+// && --- va --- and
+
+
+//  console.log(a !== b);
+//   console.log(a === b || a < b);
+//  console.log(a === b && a <= b);
+
+// let soat = 6
+// let min = 30
+
+// console.log(`Uygonaman:${soat == 5 && min == 30}`);
+// console.log(soat <= 12 && min == 30);
+// console.log(soat <= 9 && min == 30);
+
+
+// let ism = prompt("Ismingizni kiriting")
+// let age = prompt("Yoshingizni kiriting")
+// let ball = prompt("ballingizni kiriting")
+
+// let nat = ball >= 100 ? "Tabriklaymiz😍" : "Afsuss😒"
+
+// alert(`${ism},${age},${ball},${nat}`)
+
+// prompt(ism)
+// confirm(ism)
+
+
+
+
+// let age = 18
+
+// if(age > 18 ){                                      //+        
+//     console.log("Royxatdan o'ting");
+// }
+// else if(age == 18){                             // =
+//     console.log("Mumkin");
+// }
+// else{                                               //-
+//     console.log("Ro'yxatdan o'tolmaysiz");
+// }
+
+
+// let ball = 150
+// let nat = ball >= 120 ? "Qabul qilindingiz 😍👍" : "Kiraolmadingiz 😒🤔"
+
+// console.log(nat);
+
+
+
+
+
+// let name = "Sitora"
+// 1-fun
+
+// function person(name) {
+//     console.log(name);
+// }
+// person("Sitora")
+// // 2-fun
+
+// function inson(a,b) {
+//     return a + b 
+// }
+// console.log(inson(2,4));
+
+// // 3-fun EC6
+// const Plus = (c,d) => {
+//     return c * d /2
+// }
+
+// console.log(Plus(20,10));
+
+// function creat(a,b) {
+//     if(a > b){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);
+//     }
+// }
+// creat(1,50)
+
+
+// s = 20 / k ** 9
+
+
+// function getItem(son) {
+//    if(son % 2 == 0){
+//     console.log(true);
+//    }
+//    else{
+//     console.log(false);
+//    }
+
+// }
+// getItem(23);
+
+// let son = 23
+
+// if(son % 2 == 0){
+//     console.log(true);
+//    }
+//    else{
+//     console.log(false);
+//    }
+
+// console.log("Salom");
+
+
+// for(let a = 1;a <= 5;a++){
+//    console.log(`${a} salom`);
+// }
+
+// a++ IKKRIMENT
+// a--  Dekriment
+//  a += 2
+// a *= 2
+
+
+// for(let i = 1;i <= 50;i++){
+//    if(i % 2 == 0){
+//       console.log(`${i}:Juft sonlar`);
+//    }
+// }
+
+// function plus(son) {
+
+// for(let i = 1;i <= son;i++){
+//    if(i % 3 == 0 && i % 5 == 0){
+//       console.log(`${i}:5 ga xam 3 ga xam bo'linadi`);
+//    }
+//    else if(i % 5 == 0){
+//       console.log(`${i}: 5 ga bolinadi`);
+//    }
+//    else if(i % 3 == 0){
+//       console.log(`${i}:3 ga bo'linadi`);
+//    }
+//    else{
+//       console.log(`${i} bo'linmaydi`);
+//    }
+// }
+// }
+// plus(10)
+
+// function upper(a,b,c) {
+
+
+
+// }
+// upper(20,3,4)
+
+
+// let 
+// var
+// const
+
+// let son = 11
+// son = 30
+// son = 20
+// const son1 = 20
+// son1 = 20
+
+
+
+// "sting"
+// 11 - Number
+// true
+// false
+// undefined
+// null
+
+// console.log(son + son1);
+
+// !
+// ||
+// &&
+
+// let a = 20
+// let b = "20"
+
+// console.log(a === b);
+
+
+// ==
+// ===
+{/* < > */}
+
+// terneri
+// ? :  
+
+// let yosh =  aad6_$42
+
+// // spk editor
+
+// if(yosh== 16){
+//     console.log(true);
+// }
+// else if(yosh > 16){
+//     console.log("Jarimaa");
+// }
+// else{
+//     console.log(false);
+// }
+
+
+// let son = 24
+
+// if(son == 20){
+//     console.log(true);
+// }
+// else if(son > 20){
+//     console.log("Bu son katta");
+// }
+// else{
+//     console.log(false);
+// }
+
+
+// let ball = 90
+// let nat = ball > 90 ? "OK👍" : "NO😢"
+// console.log(nat);
+
+
+// if(ball >= 90 ) {
+//     console.log("OK👍");
+// }
+// else{
+//     console.log("NO😢");
+// }
+
+// let soat = 14
+// let min = 0
+
+// if(soat >= 13 && min == 30){
+//     console.log("Dars boshlandi");
+// }
+// else if(soat < 13 && min < 30){
+//     console.log("Dars boshlanmadi");
+// }
+// else{
+//     console.log("Dars tugadi");
+// }
+
+
+// dan        gacha  o'rta
+// for(let a = 20;a >= 0;a--){
+//     console.log(`${a} Salom`);
+// }
+
+
+// let b = 1
+
+// while(b <= 20){
+//     b++
+//     console.log(`${b} Salom`);
+// }
+
+// for(let i = 0; i <= 50;i++){
+//     if(i % 2 == 0){
+//         console.log(`${i} Juft son`);
+//     }
+//     if(i % 2 != 0){
+//         console.log(`${i} toq son`);
+//     }
+// }
+
+
+// let k = 2
+// let n = 10
+
+// for(let i = 0;i<= n;i++){
+
+// }
+
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+
+// let yosh = 10
+
+// if(yosh > 16) {
+//     console.log("Pasport olgansiz");
+// }
+// else if(yosh == 16){
+//     console.log("Passport oldiz");
+// }
+// else {
+//     console.log("Olmagansiz");
+// }
+
+
+// const num = [1,2,3,4,5,7,7]
+// const tar = 7
+
+// function creatAt(num,tar) {
+//     for(let i = 0;i <= num.length;i++){
+//       for(let a = i;a <= num.length;a++){
+//         if(num[i] + num[a] == tar){
+//             return[i,a]
+//         }
+//       }
+//     }
+// }
+// console.log(creatAt(num,tar));
+
+
+// let son1 = 5
+
+// for(let son = 1;son <= 10;son++){
+   
+
+// }
+// console.log(`salom ${son1}`);
+
+
+// let son = 1
+// while(son <= 10){
+//     son++
+//     console.log(`salom ${son}`);
+// }
+
+// let a = 5
+
+// for(let i = 1;i <= 5;i++){
+//    console.log((a ** i) + (a ** i));
+// }
+
+// function daraja(a,b) {
+//     for(let i = 1;i <= b;i++){
+//         console.log(a ** i);
+//     }
+// }
+// daraja(2,5)
+
+// function plus(c,d) {
+//     for(let a = 1;a <= d;a++){
+//       if([c ** a] + [c ** a] == b){
+      
+//       }
+     
+//     }
+ 
+// }
+// console.log(plus(2,5));
+
+
+// const Target = (num,tar) => {
+//     return num + tar
+// }
+// console.log(Target(4,8));
+
+
+
+function str(txt) {
+    for(let i = 0;i < txt.length;i++){
+        if(txt[i] == txt[i].toLowerCase() && txt[i].toLowerCase()){
+            console.log(txt[i].toUpperCase());
+
+        }
+        else{
+            console.log(txt[i].toLowerCase());
+        }
+    }
+}
+str("JaVascRipT")   // jAvASCrIPt
